12cfda2375fa2996693182384bedfa7231e8beb8
ericbehughes <e.hughes92@icloud.com>
2017-11-12 13:00:47 -0500

    added read speed drop down

--- views/speedreader_new.php
+++ views/speedreader_new.php
@@ -9,10 +9,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     <link rel="stylesheet" href="views/form_styling.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Speed Reader</title>
     <script type="text/javascript">
 
@@ -125,9 +126,6 @@
                         pauseBtn.addEventListener('click', onPauseClick, false);
 
                     }
-
-
-
                 };
 
 
@@ -150,15 +148,28 @@
 <div id="mainDiv" class="container" style="padding: 0">
 
     <div class="card card-container" style="width:700px">
+        <span>Hey, <?php echo $_SESSION['user_email']; ?>. You are logged in.</span>
         <a href="index.php?logout">Logout</a>
         <br>
-        <div style="width: 500px; height: 300px; background: cornflowerblue; margin: auto;display: flex; align-items: center; justify-content: center;">
 
-            <span id="currentWord" style="font-size: 40px; color: white"></span>
+        <div style="width: 500px; height: 300px; margin: auto;display: flex; align-items: center; justify-content: center;">
+
+            <span id="currentWord" style="font-size: 40px">suh</span>
         </div>
         <div style="margin: auto;display: flex; align-items: center; justify-content: center;">
             <button id="startBtn" class="btn btn-success">Start</button>
             <button id="pauseBtn" class="btn btn-warning">Pause</button>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Read Speed
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu" style="    height: auto;max-height: 100px;overflow-x: hidden;">
+                    <?php for ($i = 50; $i <= 700; $i += 50){
+                        echo '<li><a href="#">' . $i . '</a></li>';
+                    }?>
+                </ul>
+
+
+            </div>
         </div>
 
     </div>
